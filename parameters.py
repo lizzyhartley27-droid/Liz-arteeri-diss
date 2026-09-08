88095df29d8766a29c3e544ca558a68bec3674a6
liz <lizzyhartley27@gmail.com>
2026-09-08 15:18:22 +0100

    changed the value of the growth function 4.2 and patch number to 64

--- parameters.py
+++ parameters.py
@@ -20,7 +20,7 @@ master_para = {
             "IS_SIMULATION": True,  # if False then we init Simulation_obj but do not execute .full_simulation()
             "NUM_TRANSIENT_STEPS": 1000, #1000
             "NUM_RECORD_STEPS": 1000,
-            "NUM_PATCHES": 1, #100 takes too long if its 100+
+            "NUM_PATCHES": 64, #100 takes too long if its 100+
             # ----------------------------------------- #
 
             "MODEL_TIME_TYPE": "discrete",  # continuous ODEs ('continuous') or discrete maps ('discrete')?
